03ddd060dc01cc58112d6b0bd88ff4a750fc6626
qiuye1pian <qiuye1pian@sina.com>
2021-10-09 14:23:41 +0800

    创建新工程

---
+++ ConwaysGame/ConwaysGameTests/Components/CellTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConwaysGame.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGame.Components.Tests
+{
+    [TestClass()]
+    public class CellTests
+    {
+        [TestMethod()]
+        public void CellTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
