7137680d9acce5205342b9d538d7f50ca000b685
qiuye1pian <qiuye1pian@sina.com>
2021-10-09 15:04:21 +0800

    创建世界

---
+++ ConwaysGame/ConwaysGameTests/Entity/World.cs
@@ -0,0 +1,17 @@
+﻿namespace ConwaysGame.Components.Tests
+{
+    internal class World
+    {
+        private readonly int width;
+        private readonly int height;
+
+        public World(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width { get => width; }
+        public int Height { get => height; }
+    }
+}
---
+++ ConwaysGame/ConwaysGameTests/Entity/WorldTests.cs
@@ -0,0 +1,19 @@
+﻿using ConwaysGame.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConwaysGame.Components.Tests
+{
+    [TestClass()]
+    public class WorldTests
+    {
+        [TestMethod()]
+        public void 创建世界()
+        {
+            World newWorld = new World(10, 10);
+            Assert.AreEqual(10, newWorld.Width);
+            Assert.AreEqual(10, newWorld.Height);
+        }
+
+
+    }
+}
