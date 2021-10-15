83c8a33235ef278d2ee8071b9488f4e6469fa6c6
qiuye1pian <qiuye1pian@sina.com>
2021-10-15 12:17:03 +0800

    拆分展示和逻辑 试着做了一个视图层效果不太好

--- ConwaysGame/ConwaysGame/FormMain.Designer.cs
+++ ConwaysGame/ConwaysGame/FormMain.Designer.cs
@@ -30,23 +30,65 @@ namespace ConwaysGame
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_CreateWorld = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(66, 66);
+            this.panel1.BackColor = System.Drawing.Color.Coral;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(999, 798);
+            this.panel1.Size = new System.Drawing.Size(1464, 1089);
             this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button_CreateWorld);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 989);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1464, 100);
+            this.panel2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Next);
+            // 
+            // button_CreateWorld
+            // 
+            this.button_CreateWorld.Location = new System.Drawing.Point(75, 33);
+            this.button_CreateWorld.Name = "button_CreateWorld";
+            this.button_CreateWorld.Size = new System.Drawing.Size(181, 45);
+            this.button_CreateWorld.TabIndex = 3;
+            this.button_CreateWorld.Text = "生成随机世界";
+            this.button_CreateWorld.UseVisualStyleBackColor = true;
+            this.button_CreateWorld.Click += new System.EventHandler(this.button_CreateWorld_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1153, 930);
+            this.ClientSize = new System.Drawing.Size(1464, 1089);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Conway\'s Game";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -54,6 +96,9 @@ namespace ConwaysGame
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_CreateWorld;
     }
 }
 
--- ConwaysGame/ConwaysGame/FormMain.cs
+++ ConwaysGame/ConwaysGame/FormMain.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ConwaysGame.Components;
+using ConwaysGame.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,62 @@ namespace ConwaysGame
 {
     public partial class FormMain : Form
     {
+        World world;
         public FormMain()
         {
             InitializeComponent();
         }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_CreateWorld_Click(object sender, EventArgs e)
+        {
+            world = new World(50, 50);
+        }
+
+        private void RefreshPanel()
+        {
+            if (world == null) return;
+            this.panel1.Controls.Clear();
+            world.Evolution();
+            List<DisplayCell> displayCells = new List<DisplayCell>();
+            for (int x = 0; x < world.Width; x++)
+            {
+                for (int y = 0; y < world.Height; y++)
+                {
+                    displayCells.Add(new DisplayCell(world.GetCell(x, y), x, y));
+                }
+            }
+            panel1.Controls.AddRange(displayCells.ToArray());
+            panel1.Refresh();
+        }
+
+
+        private void Next(object sender, EventArgs e)
+        {
+            RefreshPanel();
+        }
+    }
+    class DisplayCell : Label
+    {
+        private static readonly int sideLength = 200;
+
+        public DisplayCell(Cell cell, int x, int y)
+        {
+            this.Width = sideLength;
+            this.Height = sideLength;
+            this.Left = sideLength * x;
+            this.Top = sideLength * y;
+            if (cell.Status == CellStatus.Alive)
+            {
+                this.BackColor = Color.AliceBlue;
+            }
+            this.Visible = true;
+        }
+
+
     }
 }
